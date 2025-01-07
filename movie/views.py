4ed20168114b3e1b6fdc99b585b4a9989efc9f3e
meytizn <zamanianmehdi23desktop@gmail.com>
2025-01-07 21:37:08 +0330

    detail view created successfully :

--- movie/views.py
+++ movie/views.py
@@ -3,9 +3,11 @@ from .models import Movie
 
 
 def movie_view(request):
-  movies=Movie.objects.all()
+  movies=Movie.objects.filter(status=True)
   return render(request,'movie/movie_list.html',{'movies':movies})
 
-
+def movie_detail(request,pk):
+  movie=Movie.objects.get(id=pk)
+  return render(request,'movie/movie_detail.html',{'movie':movie})
 
 # Create your views here.
